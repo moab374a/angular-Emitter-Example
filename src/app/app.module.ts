a8af6be1177094d454dd56f5461cee6d6b57aea8
moab374a <moab374@hotmail.com>
2022-06-13 11:37:16 +0200

    Best Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
--- src/app/posts/post-create/post-create-component.ts
+++ src/app/posts/post-create/post-create-component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {PostService} from "../post.service";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {Post} from "../post.module";
-import {response} from "express";
 
 @Component({
   selector: 'app-post',
@@ -18,18 +17,28 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   public editPost: Post
   isLoading = false;
+  form: FormGroup;
 
-  constructor(private postService: PostService, public route: ActivatedRoute , private router: Router) {
+  constructor(private postService: PostService, public route: ActivatedRoute, private router: Router) {
   }
 
 
   getSinglePost(id: string) {
     this.postService.getPost(id).subscribe(response => {
       this.editPost = response
+      this.form.setValue({
+        'title': this.editPost.title,
+        'content': this.editPost.content
+      })
     })
   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      'content': new FormControl(null, {validators: [Validators.required]})
+    })
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit'
@@ -42,28 +51,28 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onSavePost(form: NgForm) {
+  onSavePost() {
 
-    if (form.invalid) {
+    if (this.form.invalid) {
       return
     }
 
     if (this.mode == 'create') {
       this.isLoading = true;
-      this.postService.addPost(form.value.title, form.value.content).subscribe((res: void) => {
+      this.postService.addPost(this.form.value.title, this.form.value.content).subscribe((res: void) => {
         console.log(res)
         this.isLoading = false
       })
     } else {
-      this.editPost.content = form.value.content
-      this.editPost.title = form.value.title
+      this.editPost.content = this.form.value.content
+      this.editPost.title = this.form.value.title
       this.isLoading = true
       this.postService.updatePost(this.editPost).subscribe(response => {
         console.log(response)
         this.isLoading = false
       })
     }
-    form.resetForm();
+    this.form.reset();
     this.router.navigate(["/"])
   }
 
